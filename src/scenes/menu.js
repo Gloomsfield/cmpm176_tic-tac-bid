d6cfcb4774c9d2c596b8bfcf16b38b895f1216c4
Nick Marigo <118312264+Nick-Marigo@users.noreply.github.com>
2026-04-07 16:02:43 -0700

    add the rules

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -53,23 +53,39 @@ class Menu extends Phaser.Scene {
         });
 
         // Rules button
-        this.ruleButton = this.add.sprite(game.config.width / 3, game.config.height / 1.5, 'button').setInteractive().setTint(0xff6000).setScale(1.5);
+        this.ruleButton = this.add.sprite(game.config.width / 2, game.config.height / 1.5, 'button').setInteractive().setTint(0xff6000).setScale(1.5);
         this.add.bitmapText(this.ruleButton.x, this.ruleButton.y, 'winkymilky', 'Rules', 24).setOrigin(0.5).setTint(0x000000);
+        this.RuleRectangle = this.add.rectangle(game.config.width/2, game.config.height/2, 400, 450, 0xff0000, 1).setVisible(false).setDepth(5);
+        this.ruleText1 = this.add.bitmapText(this.RuleRectangle.x - 35, this.RuleRectangle.y - 200, 'winkymilky', 'Rules', 32).setTint(0x000000).setDepth(6).setVisible(false);
+        this.ruleText2 = this.add.bitmapText(this.RuleRectangle.x - 185, this.RuleRectangle.y - 150, 'winkymilky', '-Each player starts with 15 points.\n\n-A maximum of three points may\nbe placed on a tile\n\n-Each player writes down all \ninitial bids\n\n-Go through each tile and the\nplayer who bidded the most wins the tile\n(If players bid the same amount neither\nplayer gets the tile)\n\n-Once all tiles have been decided, the\nplayer who has the most three in a row\nwins.', 20).setTint(0x000000).setDepth(6).setVisible(false);
+        this.ruleTextExit = this.add.bitmapText(this.RuleRectangle.x, this.RuleRectangle.y + 200, 'winkymilky', 'Click to exit', 20).setOrigin(0.5).setTint(0x000000).setDepth(6).setInteractive().setVisible(false);
         this.ruleButton.on('pointerover', () => {
             this.ruleButton.setFrame(1);
         });
         this.ruleButton.on('pointerout', () => {
             this.ruleButton.setFrame(0);
         });
+        this.ruleButton.on('pointerdown', () => {
+            this.RuleRectangle.setVisible(true);
+            this.ruleText1.setVisible(true);
+            this.ruleText2.setVisible(true);
+            this.ruleTextExit.setVisible(true);
+        });
+        this.ruleTextExit.on('pointerdown', () => {
+            this.RuleRectangle.setVisible(false);
+            this.ruleText1.setVisible(false);
+            this.ruleText2.setVisible(false);
+            this.ruleTextExit.setVisible(false);
+        });
 
         // How to Play button
-        this.howToPlayButton = this.add.sprite(game.config.width/1.5, game.config.height / 1.5, 'button').setInteractive().setTint(0x00ff00).setScale(1.5);
+        /*this.howToPlayButton = this.add.sprite(game.config.width/1.5, game.config.height / 1.5, 'button').setInteractive().setTint(0x00ff00).setScale(1.5);
         this.add.bitmapText(this.howToPlayButton.x, this.howToPlayButton.y, 'winkymilky', 'How to play', 24).setOrigin(0.5).setTint(0x000000);
         this.howToPlayButton.on('pointerover', () => {
             this.howToPlayButton.setFrame(1);
         });
         this.howToPlayButton.on('pointerout', () => {
             this.howToPlayButton.setFrame(0);
-        });
+        });*/
     }
 }
